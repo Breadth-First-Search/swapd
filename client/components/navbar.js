e26b66c1e24bb943d84fe058a1a5a3b3c2775c1c
Ming Juan <mkjuan@gmail.com>
2020-02-04 16:30:28 -0500

    feature: submitting a search will push history to /search//query

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
@@ -7,6 +7,7 @@ import Button from '@material-ui/core/Button'
 import {makeStyles} from '@material-ui/core/styles'
 import InputBase from '@material-ui/core/InputBase'
 import SearchIcon from '@material-ui/icons/Search'
+import history from '../history'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -25,24 +26,37 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-const Navbar = ({handleClick, isLoggedIn}) => {
+const Navbar = props => {
+  const [search, setSearch] = useState('')
+  const isLoggedIn = props.isLoggedIn
+  const handleClick = props.handleClick
   const classes = useStyles()
+
+  console.log(props.user.id)
+
   return (
     <div>
       <nav>
+        {/* The navbar will show these links after you log in */}
         {isLoggedIn ? (
           <div id="navbarcontainer">
-            {/* The navbar will show these links after you log in */}
             <div id="logocontainer">
               <Link to="/">
                 <img id="swapdlogo" src="/swapd.png" />
               </Link>
-              <InputBase
-                placeholder="Search…"
-                className={classes.inputInput}
-                inputProps={{'aria-label': 'search'}}
-                onChange={console.log('asf')}
-              />
+              <form
+                onSubmit={() =>
+                  history.push(`/search/${search}/?searcherId=${props.user.id}`)
+                }
+              >
+                <InputBase
+                  placeholder="Search…"
+                  className={classes.inputInput}
+                  inputProps={{'aria-label': 'search'}}
+                  value={search}
+                  onChange={() => setSearch(event.target.value)}
+                />
+              </form>
             </div>
 
             <div className={classes.root}>
@@ -65,13 +79,19 @@ const Navbar = ({handleClick, isLoggedIn}) => {
               <Link to="/">
                 <img id="swapdlogo" src="/swapd.png" />
               </Link>
-              {/* <SearchIcon /> */}
-              <InputBase
-                placeholder="Search…"
-                className={classes.inputInput}
-                inputProps={{'aria-label': 'search'}}
-                onChange={console.log('asf')}
-              />
+              <form
+                onSubmit={() =>
+                  history.push(`/search/${search}/?searcherId=${props.user.id}`)
+                }
+              >
+                <InputBase
+                  placeholder="Search…"
+                  className={classes.inputInput}
+                  inputProps={{'aria-label': 'search'}}
+                  value={search}
+                  onChange={() => setSearch(event.target.value)}
+                />
+              </form>
             </div>
 
             <div className={classes.root}>
@@ -105,7 +125,8 @@ const Navbar = ({handleClick, isLoggedIn}) => {
  */
 const mapState = state => {
   return {
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.id,
+    user: state.user
   }
 }
 
