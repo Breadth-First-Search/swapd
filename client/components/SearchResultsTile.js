514b76de623871e88d6a73cbd424bd425a0c2dad
jasenvchan <jachan@gwu.edu>
2020-02-04 16:50:54 -0500

    partially working results page

---
+++ client/components/SearchResultsTile.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default class SearchResultsTile extends React.Component {
+  constructor() {
+    super()
+  }
+
+  componentDidMount() {}
+
+  render() {
+    let result = this.props.result
+
+    return (
+      <div>
+        <img src={result.photo} />
+        <h3>
+          {result.firstName} {result.lastName}
+        </h3>
+        <h4>Overall Rating: {result.overallRating}</h4>
+        <h4>Service Rating: {result.services[0].serviceRating}</h4>
+      </div>
+    )
+  }
+}
