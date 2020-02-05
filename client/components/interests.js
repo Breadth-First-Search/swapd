edbf18a23597dc9b7769aaaf5eed0698bc21f369
Erik Jenson <erik.a.jenson@gmail.com>
2020-02-05 17:58:03 -0500

    updates to profile edit

--- client/components/interests.js
+++ client/components/interests.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {getInterests, getUserInterests, writeInterest} from '../store/interests'
+import {
+  getInterests,
+  getUserInterests,
+  addUserInterest,
+  writeInterest
+} from '../store/interests'
 
 class Interests extends React.Component {
   constructor() {
@@ -9,13 +14,14 @@ class Interests extends React.Component {
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
-  componentDidMount() {
-    this.props.getAllInterests() // gets all interests
-    this.props.getUserInterests(this.props.user.id) //gets singe user's interests
+  async componentDidMount() {
+    await this.props.getAllInterests() // gets all interests
+    await this.props.getUserInterests(this.props.user.id) //gets singe user's interests
   }
 
   handleSubmit(evt) {
     evt.preventDefault()
+    this.props.addUserInterest(evt.target.value)
 
     //add interest to userInterest model and to userInterests on store
   }
@@ -26,39 +32,48 @@ class Interests extends React.Component {
 
   render() {
     const {allInterests, userInterests} = this.props
+    console.log('props in interests', this.props)
     return (
-      <div>
-        {/* shows a list of one user's interests. */}
-        <h4>Your Interests:</h4>
-        <div>{userInterests.map(ui => <li key={ui.id}>{ui.name}</li>)}</div>
+      userInterests && (
+        <div>
+          {/* shows a list of one user's interests. */}
+          <h4>Your Interests:</h4>
+          <div>{userInterests.map(ui => <li key={ui.id}>{ui.name}</li>)}</div>
 
-        {/* drop down list of all possible interests..ideally we want this to be a try search */}
-        <form onSubmit={this.handleSubmit}>
-          <label className="pickInterests">
-            What are your interests?
-            <select value={this.state.value} onChange={this.handleChange}>
-              {allInterests.map(i => (
-                <option key={i.id} value={i}>
-                  {i.name}
-                </option>
-              ))}
-            </select>
-            <input type="submit" value="Submit" />
-          </label>
-        </form>
-      </div>
-      //add new interest to interests table and to user's interests if unique and update for all via socket
+          {/* drop down list of all possible interests..ideally we want this to be a try search */}
+          <form onSubmit={this.handleSubmit}>
+            <label className="pickInterests">
+              Choose some interests:
+              <select value={this.state.value} onChange={this.handleChange}>
+                {allInterests.map(i => (
+                  <option key={i.id} value={i}>
+                    {i.name}
+                  </option>
+                ))}
+              </select>
+              <input type="submit" value="Submit" />
+            </label>
+          </form>
+        </div>
+      )
     )
+
+    //add new interest to interests table and to user's interests if unique and update for all via socket
   }
 }
 
 const mapStateToProps = state => {
-  return {allInterests: state.interests.all, userInterests: state.userInterests}
+  return {
+    allInterests: state.interests.all,
+    userInterests: state.interests.userInterests,
+    user: state.user
+  }
 }
 const mapDispatchToProps = dispatch => {
   return {
     getAllInterests: () => dispatch(getInterests()),
-    getUserInterests: id => dispatch(getUserInterests(id))
+    getUserInterests: id => dispatch(getUserInterests(id)),
+    addUserInterest: interest => dispatch(addUserInterest(interest))
   }
 }
 
