495f3ed7789382bf8a0ac9fba44c1f987f68803a
Ming Juan <mkjuan@gmail.com>
2020-02-13 18:01:31 -0500

    add styling

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -16,8 +16,8 @@ class UserProfile extends React.Component {
   render() {
     const {user, interests, services} = this.props
     return (
-      <div className="newUserServiceContainer">
-        <div className="leftProfile">
+      <div className="userprofilecontainer">
+        <div className="userleftProfile">
           <div style={{fontSize: '1.5em', fontWeight: 'bold'}}>
             Hi, {user.firstName}!
           </div>
